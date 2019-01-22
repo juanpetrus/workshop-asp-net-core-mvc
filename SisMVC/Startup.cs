3f4433b2fda31c77ff77101093da75c782d92efd
Juan Petrus <juan.petruss@gmail.com>
2019-01-22 08:06:23 -0200

    CRUD scaffolding

--- SisMVC/Startup.cs
+++ SisMVC/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using SisMVC.Models;
 
 namespace SisMVC
 {
@@ -33,6 +35,9 @@ namespace SisMVC
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<SisMVCContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SisMVCContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
