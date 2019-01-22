d3018a713e50cafa438ea6b1a4829204cda7ccd2
Juan Petrus <juan.petruss@gmail.com>
2019-01-22 09:56:28 -0200

    Mysql adaptation and first migration

--- SisMVC/Startup.cs
+++ SisMVC/Startup.cs
@@ -37,7 +37,8 @@ namespace SisMVC
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<SisMVCContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("SisMVCContext")));
+                    options.UseMySql(Configuration.GetConnectionString("SisMVCContext"), builder =>
+                        builder.MigrationsAssembly("SisMVC")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
