4624c7a801d7894ae752f106e2a075f3c21feffa
Juan Petrus <juan.petruss@gmail.com>
2019-01-27 13:11:04 -0200

    implement grouping search

--- SisMVC/Models/SisRecord.cs
+++ SisMVC/Models/SisRecord.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using SisMVC.Models.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace SisMVC.Models
 {
     public class SisRecord
     {
         public int Id { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double Amount { get; set; }
         public SisStatus Status { get; set; }
         public Seller Seller { get; set; }
